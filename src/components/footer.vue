c3463bd5a980d115d48a05c96b15b01647602684
ppp <974270397@qq.com>
2019-07-03 15:20:19 +0800

    完成静态页

---
+++ src/components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <p>Copyright 1998 - 2019 Blog. All Rights Reserved</p>
+  </footer>
+</template>
+
+<style scoped>
+
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+</style>
---
+++ src/pages/Edit/template.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="edit">
+    <p>编辑博客</p>
+    
+  </div>
+</template>
+
+<script src="./template.js">
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./template.css" >
+
+</style>
