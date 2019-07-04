0ec03d059df44d53a4db8b9673175557494e6b78
ppp <974270397@qq.com>
2019-07-04 15:05:23 +0800

    通过vuex完成auth部分

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,23 +4,54 @@
       <h1>Let's share</h1>
       <p>精品博客汇聚</p>
       <div class="btns">
-        <el-button>立即注册</el-button>
-        <el-button>注册账号</el-button>
+        <router-link to="/login"><el-button>立即登录</el-button></router-link>
+        <router-link to="/register"><el-button>注册账号</el-button></router-link>
       </div>
     </template>
     <template v-if="isLogin">
       <h1>Let's share</h1>
       <i class="edit el-icon-edit"></i>
-      <img class="avatar" src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt=""> 
+      <div class="user">
+        <img class="avatar" :src="user.avatar" :alt="user.username" :title="user.username">
+        <ul>
+          <li><router-link to="my">我的</router-link></li>
+          <li><a href="#" @click="onLogout">注销</a></li>
+        </ul>
+      </div>  
     </template>
   </header>
 </template>
 
 <script>
+
+import auth from '@/api/auth'
+window.auth = auth
+
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
   data() {
-    return {
-      isLogin: true
+    return {}
+  },
+
+  computed: {
+    ...mapGetters([
+      'isLogin',
+      'user'
+    ])
+  },
+
+  created() {
+    this.checkLogin()
+  },
+
+  methods: {
+    ...mapActions([
+      'checkLogin',
+      'logout'
+      ]),
+    onLogout() {
+    this.logout()
     }
   }
 }
@@ -46,7 +77,7 @@ header.no-login {
     margin: 15px 0 0 0;
     color: #fff;
   }
-
+  
   .btns {
     margin-top: 20px;
   }
@@ -54,7 +85,8 @@ header.no-login {
   button {
     margin: 20px 5px 0;
   }
-}
+} 
+
 
 header.login {
   display: flex;
@@ -79,9 +111,42 @@ header.login {
     width: 40px;
     height: 40px;
     border: 1px solid #fff;
-    border-redius: 50%;
+    border-radius: 50%;
     margin-left: 15px;
   }
+
+  .user {
+    position: relative;
+
+    ul {
+      display: none;
+      position: absolute;
+      right: 0;
+      list-style: none;
+      border: 1px solid #eaeaea;
+      margin:0;
+      padding: 0;
+      background-color: #fff;
+
+      a {
+        text-decoration: none;
+        color: #333;
+        font-size: 12px;
+        display: block;
+        padding: 5px 10px;
+
+        &:hover {
+          background-color: #eaeaea;
+        }
+      }
+
+    }
+
+    &:hover ul {
+      display: block;
+    }
+  }
 }
+
 </style>
 
